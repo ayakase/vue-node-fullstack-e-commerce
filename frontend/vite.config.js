d3d4a60282fd4b02e2988302fee76804d236ce6f
ayakase <wibu1892001@gmail.com>
2024-04-07 07:01:19 +0700

    fix build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    pluginPurgeCss({
+      variables: true,
+    }),
   ],
   resolve: {
     alias: {
